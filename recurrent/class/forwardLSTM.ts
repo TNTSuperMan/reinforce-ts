ff39cd95e4fd921d1fc8049ab5c3725c6da3bd82
TNTSuperMan <abworld@outlook.jp>
2025-05-01 13:58:13 +0900

    Model -> Prev

--- recurrent/class/forwardLSTM.ts
+++ recurrent/class/forwardLSTM.ts
@@ -1,13 +1,13 @@
 import type { Graph } from "./graph";
 import { Mat } from "./mat";
 
-interface Model{
+interface Prev{
     h: Mat[];
     c: Mat[];
     o: Mat;
 }
 
-export function forwardLSTM(G: Graph, model, hidden_sizes: number[], x: number, prev: Model): Model{
+export function forwardLSTM(G: Graph, model, hidden_sizes: number[], x: number, prev: Prev): Prev{
     if(prev == null || typeof prev.h === 'undefined') {
         var hidden_prevs = [] as Mat[];
         var cell_prevs = [] as Mat[];
