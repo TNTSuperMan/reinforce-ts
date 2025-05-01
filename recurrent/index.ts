83cfc9158ae482ab49d05fc09104804e9d77dac9
TNTSuperMan <abworld@outlook.jp>
2025-05-01 13:59:15 +0900

    update export

--- recurrent/index.ts
+++ recurrent/index.ts
@@ -7,3 +7,7 @@ export { softmax } from "./utils/softmax";
 
 export { Mat } from "./class/mat";
 export { RandMat } from "./class/randmat";
+export { forwardLSTM } from "./class/forwardLSTM";
+export { initLSTM } from "./class/initLSTM";
+
+export { Graph } from "./class/graph";
