344e2768d9a9e66e5e89a0c3e251817f03515efa
TNTSuperMan <abworld@outlook.jp>
2025-07-17 02:28:22 +0900

    make zeros function compatible

--- recurrent/utils/zeros.ts
+++ recurrent/utils/zeros.ts
@@ -1,2 +1,7 @@
-export const zeros = (n: number) =>
-    new Float64Array(n);
+export const zeros = (n: number): number[] | Float64Array => {
+    if(typeof n === "undefined" || isNaN(n)) return [];
+    if(typeof ArrayBuffer === "undefined"){
+        return Array(n).fill(0);
+    }
+    return new Float64Array(n);
+}
